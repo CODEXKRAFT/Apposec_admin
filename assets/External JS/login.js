75180a50c0fcdddd06c7ce8d7aa6834b851a4d4d
Radhakishore <radhakishoredas2k19@gmail.com>
2022-03-24 18:59:08 +0530

    fix_3

---
+++ assets/External JS/login.js
@@ -0,0 +1,44 @@
+const TOKEN_KEY='com/apposec/user/manifest/token'
+
+
+let username = document.getElementById('username')
+let userpassword = document.getElementById('userpassword')
+
+
+let previousTokenData=localStorage.getItem(TOKEN_KEY)
+if(previousTokenData!=='' && (previousTokenData!==null)){
+    window.location.href = "./index.html";
+}
+
+
+
+
+let signinevent=document.getElementById('signin')
+signinevent.addEventListener('click',()=>{
+    let user={
+        username: username.value,
+        password: userpassword.value
+    }
+    axios.post('https://apposes.herokuapp.com/adminLogin',user)
+     .then((res) => {
+        const error=res.data.error
+        if(error===0){
+            const authToken = res.data.authToken
+            localStorage.setItem(TOKEN_KEY,authToken)
+            window.location.href = "./index.html";
+
+        }else {
+            const message=res.data.message
+            alert(message)
+        }
+         
+        
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+
+})
+
+    
+
